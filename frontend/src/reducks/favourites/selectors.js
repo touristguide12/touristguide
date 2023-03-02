95159ef4d4788e55f2becd10881d67c7175d8d9e
Sheetal <ptlsheetal@yahoo.com>
2023-03-02 12:26:31 -0500

    backend

--- frontend/src/reducks/favourites/selectors.js
+++
@@ -1,6 +0,0 @@
-import { createSelector } from "reselect";
-const favouritesSelector = (state) => state.favourites;
-export const getFavourites = createSelector(
-  [favouritesSelector],
-  (state) => state.list
-);
--- frontend/src/reducks/places/selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "reselect";
-
-const placesSelector = (state) => state.places;
-
-export const getPlaces = createSelector(
-    [placesSelector],
-    state => state.list
-);
